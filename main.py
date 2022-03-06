79a06efface664ba87e283aace1f0a0f5fe28481
Shiba <phuwanat.mee@student.mahidol.ac.th>
2022-03-06 16:11:41 +0700

    'version2'

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
+from importlib.resources import path
 import cv2
+import numpy
+import face_recognition
+import os
+
+path = 'Pic'
+image=[]
+Name=[]
+Listuser= os.listdir(path)
+print(Listuser)
+for idenUser in Listuser:
+    inputImg=cv2.imread(f'{path}/{idenUser}')
+    image.append(inputImg)
+    Name.append(os.path.splitext(idenUser)[0])
+print(Name)
+
+
+def encodeImg(image):
+    encodeList=[]
+    for img in image:
+        img=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+        encode = face_recognition.encode(img)[0]
+        encodeList.append(encode)
+    return encodeList
+
+
+
+encodeKnownuser= encodeImg(image)
+print(len(encodeKnownuser))
